3c2e791b022f1f49f333d435f2a6da1b3973008e
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-07-15 16:26:02 -0400

    Replace Home and About pages with Login and Dashboard:
    - Remove `Home.tsx` and `About.tsx` components.
    - Add new `Login.tsx` for user authentication and `Dashboard.tsx` as a placeholder.
    - Update routes in `main.tsx` to reflect the changes.

---
+++ src/assets/pages/Login.tsx
@@ -0,0 +1,51 @@
+import {Button, Form, Input, Tooltip} from "@heroui/react";
+import Checkbox from "../components/Checkbox.tsx";
+import {Icon} from "@iconify-icon/react";
+import {useState} from "react";
+
+export default function Login()
+{
+    const [showPassword, setShowPassword] = useState(false);
+    return (
+        <div className={"flex flex-col items-center justify-center h-screen"}>
+            <h1 className={"text-7xl text-primary"}>Obsidian</h1>
+            <h4 className={"text-4xl"}>Server panel</h4>
+            <Form className={"mt-6 w-4/5 max-w-lg min-w-48"}>
+                <Input
+                    label={"Username"}
+                    placeholder={"Enter your username"}
+                    radius={"none"}
+                    className={"font-minecraft-body"}
+                    isRequired
+                    autoComplete={"username webauthn"}
+                    endContent={<Icon icon={"pixelarticons:users"} className={"mr-2"}/>}
+                />
+                <Input
+                    label={"Password"}
+                    placeholder={"*********"}
+                    radius={"none"}
+                    type={showPassword ? "text" : "password"}
+                    className={"font-minecraft-body"}
+                    isRequired
+                    endContent={
+                        <Tooltip content={"Toggle Password Visibility"} placement={"top"} radius={"none"} className={"font-minecraft-body"}>
+                            <Button isIconOnly size={"sm"} variant={"light"} onPress={() => setShowPassword(prev => !prev)}>
+                                <Icon icon={showPassword ? "pixelarticons:eye-closed" : "pixelarticons:eye"} width={16}/>
+                            </Button>
+                        </Tooltip>
+                    }
+                />
+                <Checkbox label={"Remember Me?"} labelPlacement={"left"} fullWidth/>
+                <Button
+                    radius={"none"}
+                    className={"font-minecraft-body mt-4 w-full"}
+                    color={"primary"}
+                    type={"submit"}
+                >
+                    Login
+                </Button>
+
+            </Form>
+        </div>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,10 @@ import ReactDOM from "react-dom/client";
 import $ from "jquery";
 
 import "./assets/css/index.css";
-import Home from "./assets/pages/Home.tsx";
-import About from "./assets/pages/About.tsx";
-import Navigation from "./assets/components/Navigation.tsx";
 import {ThemeProvider} from "./assets/providers/ThemeProvider.tsx";
 import {HeroUIProvider} from "@heroui/react";
+import Login from "./assets/pages/Login.tsx";
+import Dashboard from "./assets/pages/Dashboard.tsx";
 
 
 ReactDOM.createRoot($("#root")[0]!).render(
@@ -26,11 +25,10 @@ export function MainContentRenderer()
     const navigate = useNavigate();
     return (
         <HeroUIProvider navigate={navigate}>
-            <Navigation/>
             <Routes>
                 <Route>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/about" element={<About/>}/>
+                    <Route path="/" element={<Login/>}/>
+                    <Route path="/app" element={<Dashboard/>}/>
                 </Route>
             </Routes>
         </HeroUIProvider>
