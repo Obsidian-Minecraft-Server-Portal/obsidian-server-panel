d01888c2d0afeafc3c57c590e1d4e151f3841b49
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-07-16 15:36:29 -0400

    Add `/auth/get_users` endpoint and improve database connection handling
    - Implement `get_users` handler to retrieve user data.
    - Update all handlers to properly close database connections after use.
    - Adjust `/auth` API scope to include the new `get_users` endpoint.

--- src-actix/authentication/auth_endpoint.rs
+++ src-actix/authentication/auth_endpoint.rs
@@ -1,16 +1,19 @@
 use crate::actix_util::http_error::Result;
 use crate::app_db::open_pool;
 use crate::authentication::auth_data::{UserData, TOKEN_KEY};
-use actix_web::{post, web, HttpResponse, Responder};
+use actix_web::{get, post, web, HttpResponse, Responder};
 use serde_json::json;
 
 #[post("/")]
 pub async fn login(body: web::Json<serde_json::Value>) -> Result<impl Responder> {
-    let pool = open_pool().await?;
     let username = body.get("username").expect("Missing username").as_str().expect("Username must be a string").to_string();
     let password = body.get("password").expect("Missing password").as_str().expect("Password must be a string").to_string();
     let remember = body.get("remember").is_some_and(|v| v.as_bool().unwrap_or(false));
+
+    let pool = open_pool().await?;
     let (token, _user) = UserData::login(username, password, &pool).await?;
+    pool.close().await; // Close the database connection after use
+
     let cookie = actix_web::cookie::Cookie::build(TOKEN_KEY, &token).path("/").secure(true).http_only(true);
     let cookie = if remember { cookie.max_age(actix_web::cookie::time::Duration::days(30)) } else { cookie }.finish();
     Ok(HttpResponse::Ok().cookie(cookie).json(json!({
@@ -21,18 +24,30 @@ pub async fn login(body: web::Json<serde_json::Value>) -> Result<impl Responder>
 
 #[actix_web::put("/")]
 pub async fn register(body: web::Json<serde_json::Value>) -> Result<impl Responder> {
-    let pool = open_pool().await?;
     let username = body.get("username").expect("Missing username").as_str().expect("Username must be a string").to_string();
     let password = body.get("password").expect("Missing password").as_str().expect("Password must be a string").to_string();
+
+    let pool = open_pool().await?;
     if UserData::exists(&username, &pool).await? {
         return Ok(HttpResponse::BadRequest().json(json!({
             "error": "Username already exists",
         })));
     }
     UserData::register(username, password, &pool).await?;
+    pool.close().await;
+
     Ok(HttpResponse::Ok().json(json!({
         "message": "Registration successful",
     })))
+}
+
+#[get("/")]
+pub async fn get_users() -> Result<impl Responder> {
+    let pool = open_pool().await?;
+    let users = UserData::get_users(&pool).await?;
+    pool.close().await;
+
+    Ok(HttpResponse::Ok().json(users))
 }
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
