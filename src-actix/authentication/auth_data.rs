6712b9ba28d2c81eda07793d33c2201290b4667e
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-07-16 14:21:53 -0400

    Introduce user authentication methods in `auth_db` and enhance `UserData` structure
    - Add `login`, `register`, `get_users`, and `login_with_token` methods for user authentication.
    - Add helper functions for token generation and login time updates.
    - Update `UserData` struct to include optional `id` field, default implementation, and `FromRow` trait for database integration.
    - Improve ID hashing/unhashing with `Option` support in (de)serialization.

--- src-actix/authentication/auth_data.rs
+++ src-actix/authentication/auth_data.rs
@@ -1,35 +1,78 @@
-use serde::Deserialize;
-use sqlx::FromRow;
-use sqlx::types::chrono::{DateTime, Utc};
 use crate::authentication::user_permissions::PermissionFlag;
+use enumflags2::BitFlags;
+use serde::Deserialize;
+use sqlx::sqlite::SqliteRow;
+use sqlx::types::chrono::{DateTime, Utc};
+use sqlx::{Error, FromRow, Row};
 
 pub const TOKEN_KEY: &str = "obathtok_eP4j7XbF20KCn8k5YOjsnQ";
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, FromRow, PartialEq, Eq)]
-pub struct UserData{
-	#[serde(serialize_with = "hash_id",deserialize_with = "unhash_id")]
-	pub id: u64,
-	pub username: String,
-	#[serde(skip)]
-	pub password: String,
-	pub permissions: PermissionFlag,
-	pub join_date: DateTime<Utc>,
-	pub last_online: DateTime<Utc>,
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, PartialEq, Eq)]
+pub struct UserData {
+    #[serde(
+        serialize_with = "hash_id",
+        deserialize_with = "unhash_id",
+        skip_serializing_if = "Option::is_none"
+    )]
+    pub id: Option<u64>,
+    pub username: String,
+    #[serde(skip)]
+    pub password: String,
+    pub permissions: BitFlags<PermissionFlag>,
+    pub join_date: DateTime<Utc>,
+    pub last_online: DateTime<Utc>,
 }
 
-fn hash_id<S>(id: &u64, serializer: S) -> Result<S::Ok, S::Error>
+impl Default for UserData {
+    fn default() -> Self {
+        Self {
+            id: None,
+            username: String::new(),
+            password: String::new(),
+            permissions: PermissionFlag::None.into(),
+            join_date: Utc::now(),
+            last_online: Utc::now(),
+        }
+    }
+}
+
+fn hash_id<S>(id: &Option<u64>, serializer: S) -> Result<S::Ok, S::Error>
 where
-	S: serde::Serializer,
+    S: serde::Serializer,
 {
-	let hashed = serde_hash::hashids::encode_single(*id);
-	serializer.serialize_str(&hashed)
+    if let Some(id) = id {
+        let hashed = serde_hash::hashids::encode_single(*id);
+        serializer.serialize_str(&hashed)
+    } else {
+        serializer.serialize_none()
+    }
 }
 
-fn unhash_id<'de, D>(deserializer: D) -> Result<u64, D::Error>
+fn unhash_id<'de, D>(deserializer: D) -> Result<Option<u64>, D::Error>
 where
-	D: serde::Deserializer<'de>,
+    D: serde::Deserializer<'de>,
 {
-	let hashed = String::deserialize(deserializer)?;
-	serde_hash::hashids::decode_single(&hashed).map_err(serde::de::Error::custom)
+    let hashed = String::deserialize(deserializer)?;
+    let id = serde_hash::hashids::decode_single(&hashed).map_err(serde::de::Error::custom)?;
+    Ok(Some(id))
 }
 
+impl<'a> FromRow<'a, SqliteRow> for UserData {
+    fn from_row(row: &'a SqliteRow) -> Result<Self, Error> {
+        let id = row.try_get("id").ok();
+        let username: String = row.try_get("username")?;
+        let password: String = row.try_get("password")?;
+        let permissions: i64 = row.try_get("permissions")?;
+        let permissions = BitFlags::<PermissionFlag>::from_bits_truncate(permissions as u16);
+        let join_date: DateTime<Utc> = row.try_get("join_date")?;
+        let last_online: DateTime<Utc> = row.try_get("last_online")?;
+        Ok(UserData {
+            id,
+            username,
+            password,
+            permissions,
+            join_date,
+            last_online,
+        })
+    }
+}
--- src-actix/authentication/auth_db.rs
+++ src-actix/authentication/auth_db.rs
@@ -1,10 +1,13 @@
+use crate::authentication::auth_data::UserData;
+use anyhow::Result;
 use log::debug;
+use serde_hash::hashids::encode_single;
 use sqlx::{Executor, SqlitePool};
 
-pub async fn initialize(pool: &SqlitePool) -> anyhow::Result<()> {
-	debug!("Initializing authentication database...");
-	pool.execute(
-		r#"
+pub async fn initialize(pool: &SqlitePool) -> Result<()> {
+    debug!("Initializing authentication database...");
+    pool.execute(
+        r#"
 CREATE TABLE IF NOT EXISTS users (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	username TEXT NOT NULL UNIQUE,
@@ -14,8 +17,82 @@ CREATE TABLE IF NOT EXISTS users (
 	last_online TEXT NOT NULL DEFAULT (datetime('now'))
 );
 		"#,
-	)
-	    .await?;
-	
-	Ok(())
+    )
+    .await?;
+
+    Ok(())
+}
+
+impl UserData {
+    pub async fn login(
+        username: String,
+        password: String,
+        pool: &SqlitePool,
+    ) -> Result<(String, Self)> {
+        let user = sqlx::query_as::<_, UserData>(r#"SELECT * FROM users WHERE username = ?"#)
+            .bind(username)
+            .fetch_one(pool)
+            .await?;
+        let is_valid_password = bcrypt::verify(password, &user.password)?;
+        if !is_valid_password {
+            return Err(anyhow::anyhow!("Invalid username or password"));
+        }
+        let token = user.generate_token()?;
+        user.update_login_time(pool).await?;
+        Ok((token, user))
+    }
+    async fn login_with_token(token: &str, pool: &SqlitePool) -> Result<Option<Self>> {
+        let id_part = &token[..16];
+        let id = serde_hash::hashids::decode_single(id_part)
+            .map_err(|e| anyhow::anyhow!("Failed to decode user ID: {}", e))?;
+        let user = sqlx::query_as::<_, UserData>(r#"SELECT * FROM users WHERE id = ?"#)
+            .bind(id.to_string())
+            .fetch_optional(pool)
+            .await?;
+        if let Some(ref user) = user {
+            if !bcrypt::verify(format!("{}{}", user.username, user.password), token)? {
+                return Err(anyhow::anyhow!("Invalid token"));
+            } else {
+                user.update_login_time(pool).await?;
+            }
+        }
+        Ok(user)
+    }
+
+    pub async fn register(username: String, password: String, pool: &SqlitePool) -> Result<()> {
+        sqlx::query(r#"INSERT INTO `users` (username, password) VALUES (?, ?)"#)
+            .bind(username)
+            .bind(password)
+            .execute(pool)
+            .await?;
+        Ok(())
+    }
+    pub async fn get_users(pool: &SqlitePool) -> Result<Vec<Self>> {
+        let users = sqlx::query_as::<_, UserData>(r#"SELECT * FROM users"#)
+            .fetch_all(pool)
+            .await?;
+        Ok(users)
+    }
+
+    async fn update_login_time(&self, pool: &SqlitePool) -> Result<()> {
+        if let Some(id) = self.id {
+            sqlx::query(r#"UPDATE users SET last_online = ? WHERE id = ?"#)
+                .bind(chrono::Utc::now())
+                .bind(id.to_string())
+                .execute(pool)
+                .await?;
+        }
+
+        Ok(())
+    }
+
+    fn generate_token(&self) -> Result<String> {
+        if let Some(id) = self.id {
+            let data = format!("{}{}", self.username, self.password);
+            let tok_part = bcrypt::hash(&data, 10)?;
+            Ok(format!("{}{}", encode_single(id), tok_part))
+        } else {
+            Err(anyhow::anyhow!("User ID is not set"))
+        }
+    }
 }
