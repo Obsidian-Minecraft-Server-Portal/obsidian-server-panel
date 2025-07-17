5cf7574b124da73abe0a019f99648ab6dd7e62dc
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-07-17 11:46:06 -0400

    Enhance authentication middleware to support cookie-based tokens and bypass login/registration endpoints
    - Allow token extraction from cookies alongside headers.
    - Introduce bypass for `/api/auth/` `POST` and `PUT` methods to enable unauthenticated login and registration.

--- src-actix/authentication/auth_middleware.rs
+++ src-actix/authentication/auth_middleware.rs
@@ -1,5 +1,11 @@
+use crate::authentication::auth_data;
 use crate::authentication::auth_data::UserData;
-use actix_web::{dev::{forward_ready, Service, ServiceRequest, ServiceResponse, Transform}, error::ErrorUnauthorized, Error, HttpMessage};
+use actix_web::{
+    dev::{forward_ready, Service, ServiceRequest, ServiceResponse, Transform}, error::ErrorUnauthorized,
+    Error,
+    HttpMessage,
+};
+use awc::http::Method;
 use futures::future::LocalBoxFuture;
 use std::rc::Rc;
 
@@ -38,15 +44,22 @@ where
         let service = self.service.clone();
 
         Box::pin(async move {
-            let headers = req.headers().clone();
-            if let Some(auth_header) = headers.get("Authorization") {
-                if let Ok(token) = auth_header.to_str() {
-                    let user = UserData::authenticate_with_session_token(token).await.map_err(ErrorUnauthorized)?;
-                    req.extensions_mut().insert(user);
-                    return service.call(req).await.map_err(actix_web::error::ErrorInternalServerError);
-                }
+            let path = req.path();
+            let method = req.method();
+            if path.eq("/api/auth/") && (method == Method::POST || method == Method::PUT) {
+                // Bypass authentication for login and registration endpoints
+                return service.call(req).await.map_err(actix_web::error::ErrorInternalServerError);
             }
-
+            let cookies = req.cookie(auth_data::TOKEN_KEY);
+            let headers = req.headers().clone();
+            let token = cookies
+                .and_then(|cookie| cookie.value().parse::<String>().ok())
+                .or_else(|| headers.get("Authorization").and_then(|h| h.to_str().ok()).map(|s| s.to_string()));
+            if let Some(token) = token {
+                let user = UserData::authenticate_with_session_token(&token).await.map_err(ErrorUnauthorized)?;
+                req.extensions_mut().insert(user);
+                return service.call(req).await.map_err(actix_web::error::ErrorInternalServerError);
+            }
             Err(ErrorUnauthorized("Missing or invalid authentication token"))
         })
     }
