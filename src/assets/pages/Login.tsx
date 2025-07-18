fecaf6e0d968444631651dd2afabb317192c9b51
Drew Chase <dcmanproductions@gmail.com>
2025-07-17 22:50:10 -0400

    Enhance `Login` page with `framer-motion` animations for smoother transitions.

--- src/assets/pages/Login.tsx
+++ src/assets/pages/Login.tsx
@@ -3,83 +3,134 @@ import Checkbox from "../components/extended/Checkbox.tsx";
 import {Icon} from "@iconify-icon/react";
 import {useState} from "react";
 import {useAuthentication} from "../providers/AuthenticationProvider.tsx";
+import {AnimatePresence, motion} from "framer-motion";
 
 export default function Login()
 {
     const [showPassword, setShowPassword] = useState(false);
     const {login, isLoggingIn} = useAuthentication();
+
     return (
-        <div className={"flex flex-col items-center justify-center h-screen"}>
-            <h1 className={"text-7xl text-primary"}>Obsidian</h1>
-            <h4 className={"text-4xl"}>Server panel</h4>
-            <Form className={"mt-6 w-4/5 max-w-lg min-w-48"}
-                  onSubmit={async e =>
-                  {
-                      e.preventDefault();
-                      if (isLoggingIn) return;
-
-                      // Handle form submission logic here
-                      const data = new FormData(e.currentTarget);
-                      console.log("Submitted Login Data: ", data);
-                      const username = data.get("username") as string;
-                      const password = data.get("password") as string;
-                      const rememberMe = data.get("remeberme") === "on";
-                      try
-                      {
-                          await login(username, password, rememberMe);
-                      } catch (error: any)
-                      {
-                          addToast({
-                              title: "Login Failed",
-                              description: error.message || "An error occurred during login.",
-                              color: "danger"
-                          });
-                      }
-                  }}
-            >
-                <Input
-                    id={"login-username"}
-                    name={"username"}
-                    label={"Username"}
-                    placeholder={"Enter your username"}
-                    radius={"none"}
-                    className={"font-minecraft-body"}
-                    isRequired
-                    autoComplete={"username webauthn"}
-                    endContent={<Icon icon={"pixelarticons:users"} className={"mr-2"}/>}
-                    errorMessage={"Please provide a username or email address."}
-                />
-                <Input
-                    id={"login-password"}
-                    name={"password"}
-                    label={"Password"}
-                    placeholder={"*********"}
-                    radius={"none"}
-                    type={showPassword ? "text" : "password"}
-                    className={"font-minecraft-body"}
-                    isRequired
-                    autoComplete={"current-password webauthn"}
-                    errorMessage={"Please provide a password."}
-                    endContent={
-                        <Tooltip content={"Toggle Password Visibility"} placement={"top"} radius={"none"} className={"font-minecraft-body"}>
-                            <Button isIconOnly size={"sm"} variant={"light"} onPress={() => setShowPassword(prev => !prev)}>
-                                <Icon icon={showPassword ? "pixelarticons:eye-closed" : "pixelarticons:eye"} width={16}/>
-                            </Button>
-                        </Tooltip>
-                    }
-                />
-                <Checkbox label={"Remember Me?"} name={"remeberme"} labelPlacement={"left"} fullWidth/>
-                <Button
-                    radius={"none"}
-                    className={"font-minecraft-body mt-4 w-full"}
-                    color={"primary"}
-                    type={"submit"}
-                    isLoading={isLoggingIn}
+        <AnimatePresence>
+            <div className={"flex flex-col items-center justify-center h-screen"}>
+                <motion.h1
+                    className={"text-7xl text-primary"}
+                    initial={{opacity: 0, y: -20}}
+                    animate={{opacity: isLoggingIn ? 0 : 1, y: isLoggingIn ? -50 : 0}}
+                    exit={{opacity: 0, y: 50}}
+                    transition={{duration: 0.2}}
                 >
-                    Login
-                </Button>
+                    Obsidian
+                </motion.h1>
+                <motion.h4
+                    className={"text-4xl"}
+                    initial={{opacity: 0, y: -20}}
+                    animate={{opacity: isLoggingIn ? 0 : 1, y: isLoggingIn ? -50 : 0}}
+                    exit={{opacity: 0, y: 50}}
+                    transition={{duration: 0.2, delay: 0.1}}
+                >
+                    Server panel
+                </motion.h4>
+                <Form className={"mt-6 w-4/5 max-w-lg min-w-48"}
+                      onSubmit={async e =>
+                      {
+                          e.preventDefault();
+                          if (isLoggingIn) return;
 
-            </Form>
-        </div>
+                          const data = new FormData(e.currentTarget);
+                          const username = data.get("username") as string;
+                          const password = data.get("password") as string;
+                          const rememberMe = data.get("remeberme") === "on";
+                          try
+                          {
+                              await login(username, password, rememberMe, 500);
+                          } catch (error: any)
+                          {
+                              addToast({
+                                  title: "Login Failed",
+                                  description: error.message || "An error occurred during login.",
+                                  color: "danger"
+                              });
+                          }
+                      }}
+                >
+                    <motion.div
+                        className={"w-full"}
+                        initial={{opacity: 0, y: -20}}
+                        animate={{opacity: isLoggingIn ? 0 : 1, y: isLoggingIn ? -50 : 0}}
+                        exit={{opacity: 0, y: 50}}
+                        transition={{duration: 0.2, delay: 0.2}}
+                    >
+                        <Input
+                            id={"login-username"}
+                            name={"username"}
+                            label={"Username"}
+                            placeholder={"Enter your username"}
+                            radius={"none"}
+                            className={"font-minecraft-body"}
+                            isRequired
+                            autoComplete={"username webauthn"}
+                            endContent={<Icon icon={"pixelarticons:users"} className={"mr-2"}/>}
+                            errorMessage={"Please provide a username or email address."}
+                        />
+                    </motion.div>
+                    <motion.div
+                        className={"w-full"}
+                        initial={{opacity: 0, y: -20}}
+                        animate={{opacity: isLoggingIn ? 0 : 1, y: isLoggingIn ? -50 : 0}}
+                        exit={{opacity: 0, y: 50}}
+                        transition={{duration: 0.2, delay: 0.3}}
+                    >
+                        <Input
+                            id={"login-password"}
+                            name={"password"}
+                            label={"Password"}
+                            placeholder={"*********"}
+                            radius={"none"}
+                            type={showPassword ? "text" : "password"}
+                            className={"font-minecraft-body"}
+                            isRequired
+                            autoComplete={"current-password webauthn"}
+                            errorMessage={"Please provide a password."}
+                            endContent={
+                                <Tooltip content={"Toggle Password Visibility"} placement={"top"} radius={"none"} className={"font-minecraft-body"}>
+                                    <Button isIconOnly size={"sm"} variant={"light"} onPress={() => setShowPassword(prev => !prev)}>
+                                        <Icon icon={showPassword ? "pixelarticons:eye-closed" : "pixelarticons:eye"} width={16}/>
+                                    </Button>
+                                </Tooltip>
+                            }
+                        />
+                    </motion.div>
+
+                    <motion.div
+                        className={"w-full"}
+                        initial={{opacity: 0, y: -20}}
+                        animate={{opacity: isLoggingIn ? 0 : 1, y: isLoggingIn ? -50 : 0}}
+                        exit={{opacity: 0, y: 50}}
+                        transition={{duration: 0.2, delay: 0.4}}
+                    >
+                        <Checkbox label={"Remember Me?"} name={"remeberme"} labelPlacement={"left"} fullWidth/>
+                    </motion.div>
+
+                    <motion.div
+                        className={"w-full"}
+                        initial={{opacity: 0, y: -20}}
+                        animate={{opacity: isLoggingIn ? 0 : 1, y: isLoggingIn ? -50 : 0}}
+                        exit={{opacity: 0, y: 50}}
+                        transition={{duration: 0.2, delay: 0.5}}
+                    >
+                        <Button
+                            radius={"none"}
+                            className={"font-minecraft-body mt-4 w-full"}
+                            color={"primary"}
+                            type={"submit"}
+                            isLoading={isLoggingIn}
+                        >
+                            Login
+                        </Button>
+                    </motion.div>
+                </Form>
+            </div>
+        </AnimatePresence>
     );
 }
