88e8e235dafca40cda1b34ea6bfad3f65cc1f426
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-07-18 15:31:36 -0400

    Update `Navigation` component to improve user context and dropdown functionality
    - Add `user` validation to restrict access for unauthenticated users.
    - Enhance dropdown menu with user-specific options (Account, Settings, Logout).
    - Introduce `DropdownSection` for better menu organization.

--- src/assets/components/navigation/Navigation.tsx
+++ src/assets/components/navigation/Navigation.tsx
@@ -1,5 +1,5 @@
 import {useLocation} from "react-router-dom";
-import {Button, DropdownItem, DropdownMenu, DropdownTrigger, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@heroui/react";
+import {Button, DropdownItem, DropdownMenu, DropdownSection, DropdownTrigger, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@heroui/react";
 import {Icon} from "@iconify-icon/react";
 import {Dropdown} from "../extended/Dropdown";
 import {useAuthentication} from "../../providers/AuthenticationProvider.tsx";
@@ -8,8 +8,8 @@ import {AnimatePresence, motion} from "framer-motion";
 export default function Navigation()
 {
     const {pathname} = useLocation();
-    const {logout} = useAuthentication();
-    if (!pathname.startsWith("/app")) return null;
+    const {logout, user} = useAuthentication();
+    if (!pathname.startsWith("/app") || user == null) return null;
     return (
         <AnimatePresence>
             <Navbar maxWidth={"full"} className={"font-minecraft-body"}>
@@ -30,15 +30,15 @@ export default function Navigation()
                     transition={{duration: 0.2, delay: .2}}
                 >
                     <NavbarContent justify={"center"}>
-                        <NavbarItem as={Link} href={"/app"} className={"text-foreground flex flex-row gap-1 hover:bg-default/40 py-2 px-4 transition-background duration-200"}>
+                        <NavbarItem as={Link} href={"/app"} className={"text-foreground flex flex-row gap-1 hover:bg-default/40 py-2 px-4 transition-background duration-200 data-[active=true]:text-primary"} data-active={pathname === "/app"}>
                             <Icon icon={"pixel:home-solid"}/> <span>Home</span>
                         </NavbarItem>
                         <NavbarItem>
-                            <Dropdown showArrow >
+                            <Dropdown showArrow>
                                 <DropdownTrigger>
                                     <Button startContent={<Icon icon={"pixelarticons:map"}/>} radius={"none"} variant={"light"}>Discover</Button>
                                 </DropdownTrigger>
-                                <DropdownMenu classNames={{base: "rounded-none font-minecraft-body"}} >
+                                <DropdownMenu classNames={{base: "rounded-none font-minecraft-body"}}>
                                     <DropdownItem key={"packs"} as={Link} href={"/app/discover/packs"} className={"text-foreground"} startContent={<Icon icon={"pixelarticons:subscriptions"}/>}>Modpacks</DropdownItem>
                                     <DropdownItem key={"mods"} as={Link} href={"/app/discover/mods"} className={"text-foreground"} startContent={<Icon icon={"pixelarticons:note-multiple"}/>}>Mods</DropdownItem>
                                     <DropdownItem key={"worlds"} as={Link} href={"/app/discover/worlds"} className={"text-foreground"} startContent={<Icon icon={"pixel:globe-americas-solid"}/>}>Worlds</DropdownItem>
@@ -60,7 +60,11 @@ export default function Navigation()
                                     <Button radius={"full"} isIconOnly><Icon icon={"pixelarticons:user"}/></Button>
                                 </DropdownTrigger>
                                 <DropdownMenu classNames={{base: "rounded-none font-minecraft-body"}}>
-                                    <DropdownItem key={"logout"} startContent={<Icon icon={"pixelarticons:logout"}/>} onPress={logout}> Logout </DropdownItem>
+                                    <DropdownSection title={user.username}>
+                                        <DropdownItem key={"account"} className={"text-foreground"} startContent={<Icon icon={"pixelarticons:users"}/>} as={Link} href={`/app/user/${user.id}`}> Account </DropdownItem>
+                                        <DropdownItem key={"settings"} className={"text-foreground"} startContent={<Icon icon={"pixelarticons:sliders"}/>} as={Link} href={`/app/user/${user.id}/settings`}> Settings </DropdownItem>
+                                        <DropdownItem key={"logout"} className={"text-danger"} startContent={<Icon icon={"pixelarticons:logout"}/>} color={"danger"} onPress={logout}> Logout </DropdownItem>
+                                    </DropdownSection>
                                 </DropdownMenu>
                             </Dropdown>
                         </NavbarItem>
