ab9c707719a81dd461497a1df91b8af6158a126e
Drew Chase <dcmanproductions@gmail.com>
2025-07-17 22:50:22 -0400

    Add `framer-motion` animations to `Navigation` component for smoother interactions and transitions.

--- src/assets/components/navigation/Navigation.tsx
+++ src/assets/components/navigation/Navigation.tsx
@@ -3,6 +3,7 @@ import {Button, DropdownItem, DropdownMenu, DropdownTrigger, Link, Navbar, Navba
 import {Icon} from "@iconify-icon/react";
 import {Dropdown} from "../extended/Dropdown";
 import {useAuthentication} from "../../providers/AuthenticationProvider.tsx";
+import {AnimatePresence, motion} from "framer-motion";
 
 export default function Navigation()
 {
@@ -10,36 +11,62 @@ export default function Navigation()
     const {logout} = useAuthentication();
     if (!pathname.startsWith("/app")) return null;
     return (
-        <Navbar maxWidth={"full"} className={"font-minecraft-body"}>
-            <NavbarBrand>
-                <Link className="text-3xl text-primary font-minecraft-header" href={"/app"}>obsidian</Link>
-            </NavbarBrand>
-            <NavbarContent justify={"center"}>
-                <NavbarItem as={Link} href={"/app"} className={"text-foreground flex flex-row gap-1"}><Icon icon={"pixel:home-solid"}/> <span>Home</span></NavbarItem>
-                <NavbarItem>
-                    <Dropdown showArrow>
-                        <DropdownTrigger>
-                            <Button startContent={<Icon icon={"pixelarticons:map"}/>} radius={"none"} variant={"light"}>Discover</Button>
-                        </DropdownTrigger>
-                        <DropdownMenu classNames={{base: "rounded-none font-minecraft-body"}}>
-                            <DropdownItem key={"packs"} as={Link} href={"/app/discover/packs"} className={"text-foreground"} startContent={<Icon icon={"pixelarticons:subscriptions"}/>}>Modpacks</DropdownItem>
-                            <DropdownItem key={"mods"} as={Link} href={"/app/discover/mods"} className={"text-foreground"} startContent={<Icon icon={"pixelarticons:note-multiple"}/>}>Mods</DropdownItem>
-                        </DropdownMenu>
-                    </Dropdown>
-                </NavbarItem>
-            </NavbarContent>
-            <NavbarContent justify={"end"}>
-                <NavbarItem>
-                    <Dropdown>
-                        <DropdownTrigger>
-                            <Button radius={"full"} isIconOnly><Icon icon={"pixelarticons:user"}/></Button>
-                        </DropdownTrigger>
-                        <DropdownMenu classNames={{base: "rounded-none font-minecraft-body"}}>
-                            <DropdownItem key={"logout"} startContent={<Icon icon={"pixelarticons:logout"}/>} onPress={logout}> Logout </DropdownItem>
-                        </DropdownMenu>
-                    </Dropdown>
-                </NavbarItem>
-            </NavbarContent>
-        </Navbar>
+        <AnimatePresence>
+            <Navbar maxWidth={"full"} className={"font-minecraft-body"}>
+                <motion.div
+                    initial={{opacity: 0, y: -20}}
+                    animate={{opacity: 1, y: 0}}
+                    exit={{opacity: 0, y: -20}}
+                    transition={{duration: 0.2, delay: .1}}
+                >
+                    <NavbarBrand>
+                        <Link className="text-3xl text-primary font-minecraft-header" href={"/app"}>obsidian</Link>
+                    </NavbarBrand>
+                </motion.div>
+                <motion.div
+                    initial={{opacity: 0, y: -20}}
+                    animate={{opacity: 1, y: 0}}
+                    exit={{opacity: 0, y: -20}}
+                    transition={{duration: 0.2, delay: .2}}
+                >
+                    <NavbarContent justify={"center"}>
+                        <NavbarItem as={Link} href={"/app"} className={"text-foreground flex flex-row gap-1 hover:bg-default/40 py-2 px-4 transition-background duration-200"}>
+                            <Icon icon={"pixel:home-solid"}/> <span>Home</span>
+                        </NavbarItem>
+                        <NavbarItem>
+                            <Dropdown showArrow >
+                                <DropdownTrigger>
+                                    <Button startContent={<Icon icon={"pixelarticons:map"}/>} radius={"none"} variant={"light"}>Discover</Button>
+                                </DropdownTrigger>
+                                <DropdownMenu classNames={{base: "rounded-none font-minecraft-body"}} >
+                                    <DropdownItem key={"packs"} as={Link} href={"/app/discover/packs"} className={"text-foreground"} startContent={<Icon icon={"pixelarticons:subscriptions"}/>}>Modpacks</DropdownItem>
+                                    <DropdownItem key={"mods"} as={Link} href={"/app/discover/mods"} className={"text-foreground"} startContent={<Icon icon={"pixelarticons:note-multiple"}/>}>Mods</DropdownItem>
+                                    <DropdownItem key={"worlds"} as={Link} href={"/app/discover/worlds"} className={"text-foreground"} startContent={<Icon icon={"pixel:globe-americas-solid"}/>}>Worlds</DropdownItem>
+                                </DropdownMenu>
+                            </Dropdown>
+                        </NavbarItem>
+                    </NavbarContent>
+                </motion.div>
+                <motion.div
+                    initial={{opacity: 0, y: -20}}
+                    animate={{opacity: 1, y: 0}}
+                    exit={{opacity: 0, y: -20}}
+                    transition={{duration: 0.2, delay: .15}}
+                >
+                    <NavbarContent justify={"end"}>
+                        <NavbarItem>
+                            <Dropdown>
+                                <DropdownTrigger>
+                                    <Button radius={"full"} isIconOnly><Icon icon={"pixelarticons:user"}/></Button>
+                                </DropdownTrigger>
+                                <DropdownMenu classNames={{base: "rounded-none font-minecraft-body"}}>
+                                    <DropdownItem key={"logout"} startContent={<Icon icon={"pixelarticons:logout"}/>} onPress={logout}> Logout </DropdownItem>
+                                </DropdownMenu>
+                            </Dropdown>
+                        </NavbarItem>
+                    </NavbarContent>
+                </motion.div>
+            </Navbar>
+        </AnimatePresence>
     );
 }
