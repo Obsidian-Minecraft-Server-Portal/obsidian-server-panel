ce26def1ad91f95ac678366967eb13544ea12703
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-07-18 15:54:14 -0400

    Add `ServerList` component and integrate into `Dashboard`
    - Create `ServerList` component to display a list of servers with actions (edit, delete).
    - Integrate `ServerList` into the `Dashboard` for improved server management.

--- src/assets/pages/Dashboard.tsx
+++ src/assets/pages/Dashboard.tsx
@@ -1,15 +1,17 @@
 import {ResourceGraph} from "../components/ResourceGraph.tsx";
+import ServerList from "../components/ServerList.tsx";
 
 export default function Dashboard()
 {
     return (
-        <div className={"flex flex-col gap-4"}>
-            <div className={"flex flex-row gap-2 items-center justify-center mx-8 mt-4"}>
+        <div className={"flex flex-col gap-4 mx-8 h-full"}>
+            <div className={"flex flex-row gap-2 items-center justify-center mt-4"}>
                 <ResourceGraph variant={"cpu"} showCPUCores showHistory={true} size={"sm"} decimalPlaces={1}/>
                 <ResourceGraph variant={"memory"} unit={"gb"} size={"sm"} showMaxValue maxValue={32}/>
                 <ResourceGraph variant={"network"} unit={"mb"} isUnitOverTime={true} size={"sm"} maxValue={1000}/>
                 <ResourceGraph variant={"players"} unit={"count"} showHistory={false} size={"sm"} maxValue={10}/>
             </div>
+            <ServerList/>
         </div>
     );
 }
