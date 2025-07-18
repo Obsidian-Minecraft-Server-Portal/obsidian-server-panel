76a4db184978448f3fc18f781dd4bcff895a6e42
Drew Chase <dcmanproductions@gmail.com>
2025-07-17 22:50:00 -0400

    Add page transition animations using `framer-motion`'s `AnimatePresence`.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import {BrowserRouter, Route, Routes, useNavigate} from "react-router-dom";
+import {BrowserRouter, Route, Routes, useNavigate, useLocation} from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import $ from "jquery";
+import {AnimatePresence} from "framer-motion";
 
 import "./assets/css/index.css";
 import {ThemeProvider} from "./assets/providers/ThemeProvider.tsx";
@@ -10,7 +11,6 @@ import Login from "./assets/pages/Login.tsx";
 import Dashboard from "./assets/pages/Dashboard.tsx";
 import {AuthenticationProvider} from "./assets/providers/AuthenticationProvider.tsx";
 import Navigation from "./assets/components/navigation/Navigation.tsx";
-
 
 ReactDOM.createRoot($("#root")[0]!).render(
     <React.StrictMode>
@@ -27,6 +27,8 @@ ReactDOM.createRoot($("#root")[0]!).render(
 export function MainContentRenderer()
 {
     const navigate = useNavigate();
+    const location = useLocation();
+
     return (
         <HeroUIProvider navigate={navigate}>
             <ToastProvider
@@ -37,17 +39,19 @@ export function MainContentRenderer()
                     timeout: 3000, // 3 second timeout for toasts,
                     classNames: {
                         title: "font-minecraft-header",
-                        base: "font-minecraft-body",
+                        base: "font-minecraft-body"
                     }
                 }}
             />
-            <Navigation />
-            <Routes>
-                <Route>
-                    <Route path="/" element={<Login/>}/>
-                    <Route path="/app" element={<Dashboard/>}/>
-                </Route>
-            </Routes>
+            <Navigation/>
+            <AnimatePresence mode="wait" initial={false}>
+                <Routes location={location} key={location.pathname}>
+                    <Route>
+                        <Route path="/" element={<Login/>}/>
+                        <Route path="/app" element={<Dashboard/>}/>
+                    </Route>
+                </Routes>
+            </AnimatePresence>
         </HeroUIProvider>
     );
 }
