1d8cc4d47a50ca223cab254c4fa36503df42cd67
Drew Chase <dcmanproductions@gmail.com>
2025-07-20 13:17:33 -0400

    Initialize `server` table in database setup and enable foreign key support in SQLite configuration

--- src-actix/app_db.rs
+++ src-actix/app_db.rs
@@ -6,14 +6,19 @@ use sqlx::sqlite::{SqliteConnectOptions, SqliteJournalMode};
 pub async fn initialize_databases() -> Result<()> {
     info!("Initializing databases...");
     let pool = open_pool().await?;
-    crate::authentication::initialize(&pool).await?;
 
+    // Initialize the databases
+    crate::authentication::initialize(&pool).await?;
+    crate::server::initialize(&pool).await?;
+
+    pool.close().await;
     Ok(())
 }
 
 pub async fn open_pool() -> Result<sqlx::SqlitePool> {
     let options = SqliteConnectOptions::new()
         .journal_mode(SqliteJournalMode::Wal)
+        .foreign_keys(true)
         .filename("app.db")
         .log_statements(LevelFilter::Trace)
         .create_if_missing(true);
