2f48b8f991bec4adc6f85e9ebfc56922af209011
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-07-28 12:08:59 -0400

    Add `FileTableBreadcrumbs` component for path navigation
    - Introduce `FileTableBreadcrumbs` with `onNavigate` callback and dynamic path rendering.
    - Enhance user experience with intuitive breadcrumb support in file navigation.

---
+++ src/assets/components/server-components/server-page/files/FileTableBreadcrumbs.tsx
@@ -0,0 +1,44 @@
+import {BreadcrumbItem, Breadcrumbs} from "@heroui/react";
+
+type FileTableBreadcrumbsProperties = {
+    onNavigate: (path: string) => void;
+    paths: string[];
+}
+
+export function FileTableBreadcrumbs(props: FileTableBreadcrumbsProperties)
+{
+    const {paths, onNavigate} = props;
+
+    return (
+        <Breadcrumbs variant={"bordered"}>
+            <BreadcrumbItem
+                key="root"
+                onPress={() =>
+                {
+                    onNavigate("");
+                }}
+            >
+                Root
+            </BreadcrumbItem>
+            {(!paths.length || paths.every(p => p.trim() === "")) ? (
+                <></>
+            ) : (
+                paths.filter(i => i.trim() !== "").map((path, index) => (
+                    <BreadcrumbItem
+                        key={path}
+                        onPress={() =>
+                        {
+                            if (index >= paths.length)
+                                return;
+                            let newPath = paths.slice(0, index + 1).join("/");
+                            if (!newPath.endsWith("/")) newPath += "/";
+                            onNavigate(newPath);
+                        }}
+                    >
+                        {path === "" || path === "/" ? "Root" : path}
+                    </BreadcrumbItem>
+                ))
+            )}
+        </Breadcrumbs>
+    );
+}
