a77d5a960b301af3f149d7ccf8bfd88c86359208
Drew Chase <dcmanproductions@gmail.com>
2025-07-27 21:49:04 -0400

    Add tab state handling with URL sync and enhance `ServerPage` tabs
    - Introduced state management for selected tab using `useState` and `useSearchParams` to sync tab state with the URL query parameter.
    - Updated `Tabs` component to handle `onSelectionChange` and dynamic tab keys for better interaction and routing.
    - Added `ServerFiles` component integration for the "Files" tab.

--- src/assets/pages/ServerPage.tsx
+++ src/assets/pages/ServerPage.tsx
@@ -1,14 +1,35 @@
-import {useParams} from "react-router-dom";
-import {useEffect} from "react";
+import {useParams, useSearchParams} from "react-router-dom";
+import {useEffect, useState} from "react";
 import {useServer} from "../providers/ServerProvider.tsx";
 import {ServerHeader} from "../components/server-components/server-page/ServerHeader.tsx";
 import {Tab, Tabs} from "@heroui/react";
-import ServerConsole from "../components/server-components/server-page/ServerConsole.tsx";
+import ServerConsole from "../components/server-components/server-page/console/ServerConsole.tsx";
+import {ServerFiles} from "../components/server-components/server-page/files/ServerFiles.tsx";
 
 export default function ServerPage()
 {
     const {server, loadServer, unloadServer} = useServer();
+    const [selectedTab, setSelectedTab] = useState("console");
     const {id} = useParams();
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    useEffect(() =>
+    {
+        const tab = searchParams.get("tab");
+        if (tab && ["console", "content", "files", "backups", "options"].includes(tab))
+        {
+            setSelectedTab(tab);
+        } else
+        {
+            setSelectedTab("console");
+        }
+
+    }, [searchParams]);
+
+    useEffect(() =>
+    {
+        setSearchParams({tab: selectedTab, ...searchParams}, {replace: true});
+    }, [selectedTab]);
 
     useEffect(() =>
     {
@@ -29,12 +50,12 @@ export default function ServerPage()
     return (
         <div className={"flex flex-col gap-4 px-8"}>
             <ServerHeader id={id} name={server.name} minecraft_version={server.minecraft_version} server_type={server.server_type} loader_version={server.loader_version} status={server.status}/>
-            <Tabs className={"mt-4 font-minecraft-body"} radius={"none"} color={"primary"}>
-                <Tab title={"Console"}><ServerConsole/></Tab>
-                <Tab title={"Content"}/>
-                <Tab title={"Files"}/>
-                <Tab title={"Backups"}/>
-                <Tab title={"Options"}/>
+            <Tabs className={"mt-4 font-minecraft-body"} radius={"none"} color={"primary"} onSelectionChange={value => setSelectedTab(value as string)} selectedKey={selectedTab}>
+                <Tab key={"console"} title={"Console"}><ServerConsole/></Tab>
+                <Tab key={"content"} title={"Content"}/>
+                <Tab key={"files"} title={"Files"}><ServerFiles/></Tab>
+                <Tab key={"backups"} title={"Backups"}/>
+                <Tab key={"options"} title={"Options"}/>
             </Tabs>
         </div>
     );
