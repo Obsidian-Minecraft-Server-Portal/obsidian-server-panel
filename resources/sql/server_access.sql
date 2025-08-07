a0f4dab4af6e367e67bbc27f8843baea3bf3a479
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-08-07 09:19:49 -0400

    Remove `server_access` table as part of server access model cleanup

--- resources/sql/server_access.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `server_access`
-(
-    `id`        INTEGER unsigned PRIMARY KEY AUTOINCREMENT,
-    `server_id` INTEGER unsigned NOT NULL,
-    `user_id`   INTEGER unsigned NOT NULL
-)
